27b950c79db15542fc8aaa9da2fb9a81579db1a1
jaem1n207 <tech.jmtt@gmail.com>
2025-08-30 05:13:20 +0900

    feat: sentry 통합 및 설정 추가
    - Sentry 설정을 위한 .env.sentry-build-plugin 파일 추가
    - next.config.ts 파일에서 Sentry 설정을 적용하여 에러 추적 및 모니터링 기능 강화

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://d37773402f8b8690700c3fda3881d323@o4509283258990592.ingest.us.sentry.io/4509929025634304',
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
---
+++ src/instrumentation-client.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://d37773402f8b8690700c3fda3881d323@o4509283258990592.ingest.us.sentry.io/4509929025634304',
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
