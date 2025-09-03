cd9b4d8d5efb107acd8b3bc889f120e4e1f835a3
jaem1n207 <tech.jmtt@gmail.com>
2025-09-03 19:03:47 +0900

    chore: apple-icon.tsx에서 runtime을 'nodejs'로 변경 & @next/eslint-plugin-next 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    ignorePatterns: [
       'node_modules/**',
       '.next/**',
       'out/**',
@@ -22,8 +20,6 @@ const eslintConfig = [
       'next-env.d.ts',
       '*.config.*',
     ],
-  },
-  {
     rules: {
       'import/order': [
         'error',
@@ -97,7 +93,7 @@ const eslintConfig = [
         },
       ],
     },
-  },
+  }),
 ]
 
 export default eslintConfig
