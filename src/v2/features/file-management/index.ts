be246ae8b9a7914808eef5ea398689092ccc32fb
jaem1n207 <tech.jmtt@gmail.com>
2026-04-26 18:00:40 +0900

    feat: v2 zip 저장 경로 추가

---
+++ src/v2/features/file-management/index.ts
@@ -0,0 +1,12 @@
+export {
+  buildImageFileName,
+  buildTextFileName,
+  buildTruckFolderName,
+} from './filename'
+export {
+  isFileSystemAccessAvailable,
+  saveTruckToDirectory,
+} from './file-system'
+export type { WritableDirectoryHandle } from './file-system'
+export { buildTruckTextContent } from './text-content'
+export { createTruckZipBlob, downloadTruckZip } from './zip-fallback'
